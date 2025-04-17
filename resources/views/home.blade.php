d64e6899501b3a1f3f67a66b9b57cd161240efa7
kurakirakk <amardesu5@gmail.com>
2025-04-17 15:36:24 +0800

    typo fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,13 +9,16 @@
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.17/multimonth@6.1.17/index.global.min.js'></script>
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.17/list@6.1.17/index.global.min.js'></script>
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.17/interaction@6.1.17/index.global.min.js'></script>
+    <script src='fullcalendar/dist/index.global.js'></script>
     <script>
 
       document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
           initialView: 'interaction',
-          initialView: 'multiMonthYear'        
+          initialView: 'multiMonthYear',
+          selectable: true      
+          
         });
         calendar.render();
       });
