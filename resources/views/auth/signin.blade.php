96b4729630c102f2bf909d685ed52f3cbabb8c86
kurakirakk <amardesu5@gmail.com>
2025-05-19 20:25:33 +0800

    csrf error fix

--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -1,10 +1,30 @@
-<x-guest-layout>
-    {{-- <div class="container position-sticky z-index-sticky top-0">
-        <div class="row">
-            <div class="col-12">
-                <x-guest.sidenav-guest />
-            </div>
-        </div> --}}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <title>
+        E-Rapat
+    </title>
+    <!--     Fonts and icons     -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Noto+Sans:300,400,500,600,700,800|PT+Mono:300,400,500,600,700"
+        rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/349ee9c857.js" crossorigin="anonymous"></script>
+    <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="../assets/css/corporate-ui-dashboard.css?v=1.0.0" rel="stylesheet" />
+</head>
+
+<body class="">
+
     </div>
     <main class="main-content  mt-0">
         <section>
@@ -15,27 +35,15 @@
                             <div class="card card-plain mt-8">
                                 <div class="card-header pb-0 text-left bg-transparent text-center">
                                     <h3 class="font-weight-black text-dark display-6">LOGIN</h3>
-                                    {{-- <p class="mb-0">Selamat Datang di E-RAPAT </p>
-                                    <p class="mb-0">DISKOMINFO KABUPATEN BADUNG!</p> --}}
-                                    {{-- <p class="mb-0">Silahkan masukkan username dan password.<br></p> --}}
+                                    
+                                    
                                     
                                 </div>
                                 <div class="text-center">
-                                    @if (session('status'))
-                                        <div class="mb-4 font-medium text-sm text-green-600">
-                                            {{ session('status') }}
-                                        </div>
-                                    @endif
-                                    @error('message')
-                                        <div class="alert alert-danger text-sm" role="alert">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
-                                </div>
+                                                                                                        </div>
                                 <div class="card-body">
                                     <form role="form" class="text-start" method="POST" action="sign-in">
-                                        @csrf
-                                      
+                                        <input type="hidden" name="_token" value="yP1dgXCoYTyjz16rz2zBv4ksneEtO8YFsTg7BVIC" autocomplete="off">                                      
 
                                         <label>Username</label>
                                         <div class="mb-3">
@@ -49,7 +57,7 @@
                                         <label>Password</label>
                                         <div class="mb-3">
                                             <input type="password" id="password" name="password"
-                                                {{-- value="{{ old('password') ? old('password') : 'secret' }}" --}}
+                                                
                                                 class="form-control" placeholder="Enter password" aria-label="Password"
                                                 aria-describedby="password-addon">
                                         </div>
@@ -61,7 +69,7 @@
                                                     Remember me
                                                 </label>
                                             </div>
-                                            <a href="{{ route('password.request') }}"
+                                            <a href="http://127.0.0.1:8000/forgot-password"
                                                 class="text-xs font-weight-bold ms-auto">Forgot
                                                 password?</a>
                                         </div>
@@ -94,5 +102,24 @@
             </div>
         </section>
     </main>
+    <!--   Core JS Files   -->
+    <script src="../assets/js/core/popper.min.js"></script>
+    <script src="../assets/js/core/bootstrap.min.js"></script>
+    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <script>
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        }
+    </script>
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    
+    <script src="../assets/js/corporate-ui-dashboard.min.js?v=1.0.0"></script>
+</body>
 
-</x-guest-layout>
+</html>
